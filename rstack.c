7a217e68295cbd16424ab7699c8d9695bfe218f5
JesseDebian <johansgate@gmail.com>
2026-04-12 16:39:32 +0200

    final fixes

--- rstack.c
+++ rstack.c
@@ -383,33 +383,3 @@ int rstack_write(char const *path, rstack_t *rs) {
 
     return FUNCTION_SUCCESS;
 }
-
-static int wojtekmal_0(void) {
-    rstack_t *rs0 = rstack_new();
-    rstack_t *rs1 = rstack_new();
-    rstack_t *rs2 = rstack_new();
-
-    rstack_push_value(rs0, 3);
-    rstack_push_value(rs0, 2);
-    rstack_push_value(rs0, 1);
-    rstack_push_rstack(rs0, rs1);
-
-    rstack_push_value(rs1, 5);
-    rstack_push_rstack(rs1, rs2);
-    rstack_push_value(rs1, 4);
-
-    rstack_push_rstack(rs2, rs1);
-    rstack_push_value(rs2, 6);
-
-    rstack_write("w.out", rs0);
-
-    rstack_delete(rs0);
-    rstack_delete(rs1);
-    rstack_delete(rs2);
-
-    return true;
-}
-
-int main() {
-    wojtekmal_0();
-}
